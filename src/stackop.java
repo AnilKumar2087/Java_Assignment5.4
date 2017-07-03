14b34cbac178a48cf4e518bd92d6878eb31b39a5
AnilKumar2087 <a.anil.adunuri@gmail.com>
2017-07-03 23:47:17 +0530

    assignemnt

---
+++ src/stackop.java
@@ -0,0 +1,11 @@
+/*
+ * We implement the interface to perform stack operation. 
+ * The Stack interface creates a fixed length stack.
+ */
+
+public interface stackop {
+	
+    void push(int item);
+    int pop();   
+    
+} // end of interface
